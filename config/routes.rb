2c3fbff6b677a14605966394d6280f0d52dd0930
Andrew Dotterer <adotterer@gmail.com>
2021-04-07 17:54:50 -0700

    react is workinggit add .

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'cherry_pie', to: 'cherry_pie#index'
+  get 'hello_world', to: 'hello_world#index'
+  get 'cherry_pie', to: 'cherry_pie#index'
   resources :microposts
   resources :users
-  get 'application#hello'
+  root 'application#hello'
   resource :welcome, only: [:index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
